a905752c1296af48abf0cbc18bf46bc0a9e02414
Leon Prouger <leonprou@gmail.com>
2019-11-01 08:39:33 +0200

    deduce content type for images

--- app/controllers/ImageController.js
+++ app/controllers/ImageController.js
@@ -1,3 +1,5 @@
+const imageType = require('image-type')
+
 module.exports = (osseus) => {
   return {
     create: async (req, res, next) => {
@@ -12,7 +14,8 @@ module.exports = (osseus) => {
     get: async (req, res, next) => {
       osseus.lib.Metadata.get(req.params.hash, true)
         .then(md => {
-          res.set('Content-Type', 'image')
+          const type = imageType(md.data)
+          res.set('Content-Type', type ? type.mime : 'image/png')
           res.send(md.data)
         })
         .catch(err => { next(err) })
